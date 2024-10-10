ce759876e6e82da3c4fde911428a178a000a847c
Tutur <81999182+HTMLArthurHTML@users.noreply.github.com>
2024-10-10 17:11:35 +0200

    Update syllogism.h

--- syllogism.h
+++ syllogism.h
@@ -19,8 +19,8 @@ typedef struct
     bool affirmative;
 }analysis_proposition;
 
-user_proposition user_syllogism[3];
-analysis_proposition analysis_syllogism[3];
+// user_proposition user_syllogism[3];
+// analysis_proposition analysis_syllogism[3];
 
 T_liste add_quantifier(const char * quantifier, T_liste  ql );
 void display_quantifier(const T_liste  ql);
