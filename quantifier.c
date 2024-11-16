45c4e5422e85863136efb699822af91970e21c51
BerArth <arthur.bertrand16@hotmail.fr>
2024-11-16 13:38:11 +0100

    16/11

--- quantifier.c
+++ quantifier.c
@@ -10,9 +10,8 @@ T_quantifier uq5 = {"Nul", true, false};
 //existential quantifier
 T_quantifier eq1 = {"Certains", false, true};
 T_quantifier eq2 = {"Il existe", false, true};
-T_quantifier eq3 = {"Il existe", false, true};
-T_quantifier eq4 = {"Il n'y a pas", false, false};
-T_quantifier eq5 = {"Quelques", false, true};
+T_quantifier eq3 = {"Il n'y a pas", false, false};
+T_quantifier eq4 = {"Quelques", false, true};
 
 
 T_liste generate_list_quanti_u(T_liste l){
@@ -32,7 +31,6 @@ T_liste generate_list_quanti_e(T_liste l){
     l = add_quantifier(eq2, l);
     l = add_quantifier(eq3, l);
     l = add_quantifier(eq4, l);
-    l = add_quantifier(eq5, l);
 
     return l;
 }
