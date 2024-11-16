608357e86b5a831e9fb8e5c1a815e6df3cd6e54d
BerArth <arthur.bertrand16@hotmail.fr>
2024-11-16 13:40:38 +0100

    16/11

--- quantifier.h
+++ quantifier.h
@@ -1,4 +1,9 @@
+#ifndef QUANTIFIER_H_INCLUDED
+#define QUANTIFIER_H_INCLUDED
+
 #include "syllogism.h"
 
 T_liste generate_list_quanti_u(T_liste l);
 T_liste generate_list_quanti_e(T_liste l);
+
+#endif
